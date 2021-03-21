3782a2eb1214b1dd541abd95a3ac38121f56b57a
worldesing <worldesigday@gmail.com>
2021-03-21 11:51:21 -0500

    ajuste slider responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,20 @@
 var swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  slidesPerView: 2,
-  spaceBetween: 30,
+
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
   breakpoints: {
     // when window width is >= 576px
     0: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      }
+      slidesPerView: 'auto',
+      spaceBetween: 20
     },
     // when window width is >= 768px
     567: {
